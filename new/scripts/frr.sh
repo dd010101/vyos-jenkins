7f935b8bfa633917b829cb390b04de1fe9e81fd9
dd <dd@wx.tnyzeq.icu>
2025-11-12 15:42:37 +0100

    updated frr build to replace libyang3 with libyang2 as temporary workaround to make official frr build produce valid set of packages

--- new/scripts/frr.sh
+++ new/scripts/frr.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 set -e
+
 sudo apt-get update
 sudo apt-get install -y bison flex libelf-dev:native libreadline-dev liblua5.3-dev
+
+# temporary workaround, official build script doesn't work lib libyang3 currently
+# so we need to patch it back to libyang2 to make the official build script work
+awk '
+BEGIN { pkg = "" }
+/^\[\[packages\]\]/ { pkg = "" }
+/^name *= *"libyang"/ { pkg = "libyang" }
+/^commit_id *= *"v3/ && pkg == "libyang" {
+    sub(/v3[^"]*/, "v2.1.148")
+}
+{ print }
+' package.toml > package.toml.tmp
+mv package.toml.tmp package.toml
+
 python3 ./build.py
