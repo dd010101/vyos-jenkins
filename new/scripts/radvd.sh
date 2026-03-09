b0cf7c82cb9b0053cdaa75943744fcc4c61fb48b
dd <dd@wx.tnyzeq.icu>
2026-03-09 11:46:18 +0100

    fixed circinus radvd silent failed build

---
+++ new/scripts/radvd.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt-get update
+sudo apt-get install -y byacc flex
+
+if [ -f ./build.py ]; then
+  python3 ./build.py
+else
+  /my-build-scripts/generic-build-script.sh
+fi
